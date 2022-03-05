7eed1b173dc9b91cab61253a1e4a45c1902cb3c1
LeMO <lmo9903@naver.com>
2022-03-05 21:07:19 +0900

    add index.html, edit startApplication

--- spring/start/src/main/java/iducs/springboot/start/StartApplication.java
+++ spring/start/src/main/java/iducs/springboot/start/StartApplication.java
@@ -2,9 +2,17 @@ package iducs.springboot.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class StartApplication {
+    @GetMapping("/spring")
+    public String spring() {
+        return "hello";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class, args);
