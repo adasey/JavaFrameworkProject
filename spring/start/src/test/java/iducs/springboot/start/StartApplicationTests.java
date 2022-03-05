b8c51a1d81bb3879e2c7b5cc67208cb3bf905c54
LeMO <lmo9903@naver.com>
2022-03-05 20:17:02 +0900

    add spring project start

---
+++ spring/start/src/test/java/iducs/springboot/start/StartApplicationTests.java
@@ -0,0 +1,13 @@
+package iducs.springboot.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
