be02f54b55c181115c292cdacb67c1ea6046e68b
LeMO <lmo9903@naver.com>
2022-03-31 18:07:59 +0900

    boot-jpa 기능 추가

--- spring/boot-jpa/src/main/java/iducs/springboot/bootjpa/controller/MemoController.java
+++ spring/boot-jpa/src/main/java/iducs/springboot/bootjpa/controller/MemoController.java
@@ -1,13 +1,34 @@
 package iducs.springboot.bootjpa.controller;
 
+import iducs.springboot.bootjpa.entity.MemberEntity;
+import iducs.springboot.bootjpa.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
 
 // 모든 페이지는 controll을 통해 접근
 @Controller
 public class MemoController {
-    @GetMapping("/") // default setting 대신 /index를 통해서 접근하도록 설정 기본은 그냥 localhost:8888에서 index로 접근
-    public String getIndex(){
+    @Autowired
+    MemberService memberService;
+
+    @GetMapping("/tables") // default setting 대신 /index를 통해서 접근하도록 설정 기본은 그냥 localhost:8888에서 index로 접근
+    public String getIndex(Model model) {
+        List<MemberEntity> list = memberService.readAll();
+        model.addAttribute("members", list);
+        return "tables";
+    }
+
+    @GetMapping("/{idx}") // default setting 대신 /index를 통해서 접근하도록 설정 기본은 그냥 localhost:8888에서 index로 접근
+    public String getMember(@PathVariable("idx") Long seq, Model model) {
+        Optional<MemberEntity> member = memberService.readById(seq);
+        MemberEntity mem = member.get();
+        model.addAttribute("member", mem);
         return "index";
     }
 
--- spring/boot-jpa/src/test/java/iducs/springboot/bootjpa/BootJpaApplicationTests.java
+++ spring/boot-jpa/src/test/java/iducs/springboot/bootjpa/BootJpaApplicationTests.java
@@ -1,13 +1,36 @@
 package iducs.springboot.bootjpa;
 
+
+import iducs.springboot.bootjpa.entity.MemberEntity;
+import iducs.springboot.bootjpa.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.IntStream;
+
 
 @SpringBootTest
 class BootJpaApplicationTests {
 
+    // POJO (Plain Old Java Object)
+    @Autowired
+    MemberRepository memberRepository;
+
     @Test
     void contextLoads() {
-    }
+        IntStream.rangeClosed(1, 10).forEach(i -> {
+            MemberEntity member =
+                    MemberEntity.builder().id("id" + i).pw("pw" + i).name("name" + i).build();
+            memberRepository.save(member); // insert 문
 
+//            member = MemberEntity.builder().pw("pw" + i).build();
+//            member = MemberEntity.builder().name("name" + i).build();
+//            member = MemberEntity.builder().pw("email" + i).build();
+//            member = MemberEntity.builder().pw("phone" + i).build();
+//            member = MemberEntity.builder().pw("pw" + i).build();
+//            member = MemberEntity.builder().pw("pw" + i).build();
+//            memoRepository.save(memo);
+        });
+    }
 }
