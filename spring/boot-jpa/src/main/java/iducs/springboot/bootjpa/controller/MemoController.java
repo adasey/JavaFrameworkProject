db6f25cc27e498459e144153bf669a3650288b9f
LeMO <lmo9903@naver.com>
2022-05-03 17:18:30 +0900

    java school project memo delete add

--- spring/boot-jpa/src/main/java/iducs/springboot/bootjpa/controller/MemoController.java
+++ spring/boot-jpa/src/main/java/iducs/springboot/bootjpa/controller/MemoController.java
@@ -1,6 +1,7 @@
 package iducs.springboot.bootjpa.controller;
 
 
+import iducs.springboot.bootjpa.domain.Member;
 import iducs.springboot.bootjpa.domain.Memo;
 import iducs.springboot.bootjpa.entity.MemoEntity;
 import iducs.springboot.bootjpa.service.MemoService;
@@ -22,41 +23,55 @@ public class MemoController {
     @GetMapping("/regform")
     public String getRegform(Model model) {
         model.addAttribute("memo", Memo.builder().build());
-        return "/memos/regform";
+        return "memos/regform";
     }
 
     @PostMapping("")
     public String postMemo(@ModelAttribute("memo") Memo memo, Model model) {
         memoService.create(memo);
         model.addAttribute("memo", memo);
-        return "/memos/memo";
+        return "memos/memo";
     }
 
     @GetMapping("/")
     public String getMemos(Model model) {
         List<Memo> memos = memoService.readAll();
         model.addAttribute("list", memos);
-        return "/memos/home";
+        return "memos/home";
     }
 
     @GetMapping("/{idx}")
     public String getMemo(@PathVariable("idx") Long mno, Model model) {
         Memo memo = memoService.readById(mno);
         model.addAttribute("memo", memo);
-        return "/memos/memo";
+        return "memos/memo";
     }
 
     @GetMapping("/{idx}/upform")
     public String getUpform(@PathVariable("idx") Long mno, Model model) {
         Memo memo = memoService.readById(mno);
         model.addAttribute("memo", memo);
-        return "/memos/upform";
+        return "memos/upform";
     }
 
     @PutMapping("/{idx}")
     public String putMemo(@ModelAttribute("memo") Memo memo, Model model) {
         memoService.update(memo);
         model.addAttribute("memo", memo);
-        return "/memos/memo";
+        return "memos/memo";
+    }
+
+    @GetMapping("/{idx}/delform")
+    public String getDelform(@PathVariable("idx") Long mno, Model model) {
+        Memo memo = memoService.readById(mno);
+        model.addAttribute("memo", memo);
+        return "memos/delform";
+    }
+
+    @DeleteMapping("/{idx}")
+    public String delMemo(@ModelAttribute("memo") Memo memo, Model model) {
+        memoService.delete(memo);
+        model.addAttribute("memo", memo);
+        return "redirect:memos/memo"; // memos/info에 새롭게
     }
 }
